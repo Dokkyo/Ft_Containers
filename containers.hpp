ab2b5aaf770cb6339ee1a3ad455e4516ef4b753b
naben-za <nabil.piscine42@gmail.com>
2023-02-08 20:24:02 +0100

    in proc

--- containers.hpp
+++ containers.hpp
@@ -3,10 +3,47 @@
 
 #include <memory>
 #include <iostream>
+#include <cstddef>
 
 #include "vector.hpp"
 #include "stack.hpp"
 #include "map.hpp"
+
+template <class Iterator>
+class iterator_traits : public std::iterator
+{
+    typedef Iterator::difference_type   difference_type;
+    typedef Iterator::value_type        value_type;
+    typedef Iterator::pointer           pointer;
+    typedef Iterator::reference         reference;
+    typedef Iterator::iterator_category iterator_category;
+};
+
+template <class T>
+class iterator_traits<T*> : public std::iterator
+{
+    typedef std::iterator::difference_type   ptrdiff_t;
+    typedef std::iterator::value_type        T;
+    typedef std::iterator::pointer           T*;
+    typedef std::iterator::reference         T&;
+    typedef std::iterator::iterator_category std::random_access_iterator_tag;
+};
+
+template <class T>
+class iterator_traits<const T*> : public std::iterator
+{
+    typedef std::iterator::difference_type   ptrdiff_t;
+    typedef std::iterator::value_type        T;
+    typedef std::iterator::pointer           T* const;
+    typedef std::iterator::reference         T& const;
+    typedef std::iterator::iterator_category std::random_access_iterator_tag;
+};
+
+template <class T>
+struct is_integral
+{
+
+};
 
 namespace ft
 {
--- vector.hpp
+++ vector.hpp
@@ -5,16 +5,41 @@ namespace ft
     template < class T, class Alloc = std::allocator<T> >
     class vector
     {
-        typedef T value_type;
-        typedef Alloc allocator_type;
+        typedef T                   value_type;
+        typedef Alloc               allocator_type;
+        typedef size_t              size_type;
+        // typedef value_type*         pointer;
+        // typedef value_type*         const_pointer;
+        // typedef value_type&         reference;
+        // typedef value_type&         const_reference;
 
         public:
 
             // Constructors
-            explicit vector(const allocator_type& alloc = allocator_type()) : _tab(new T) {
-                std::cout << "vector default constructor called\n";
+            explicit vector(const allocator_type& alloc = allocator_type()) : _tab(get_allocator().allocate(0)) {
+                std::cout << "vector first constructor called\n";
+                // _tab = get_allocator().allocate(0);
                 _copy = alloc;
             };
+            explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _tab(get_allocator().allocate(n)){
+                size_t i = -1;
+                std::cout << "vector second constructor called\n";
+                _copy = alloc;
+                // _tab = get_allocator().allocate(n);
+                while (++i < n)
+                    _tab[i] = val;
+                    // get_allocator().construct(_tab,val);
+                i = -1;
+                while (++i < n)
+                    std::cout << i << "=" << _tab[i] << "\n";
+            };
+            template <class InputIterator>
+            vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {
+
+            };
+            // vector (const vector& x){
+
+            // };
 
             // Allocator
             allocator_type get_allocator() const {
@@ -22,7 +47,7 @@ namespace ft
             };
 
         private:
-            allocator_type   _copy;
-            T               *_tab;
+            allocator_type  _copy;
+            T*              _tab;
     };
 }
